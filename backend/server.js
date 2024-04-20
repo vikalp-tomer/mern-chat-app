91d9c2262794dbeeeaf9d2ae200a12d77c741075
vikalp-tomer <vikalptomar15@gmail.com>
2024-04-20 21:25:35 +0530

    complete backend without socket.io implementation

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -10,8 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToDB();
