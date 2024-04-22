0a0cc3c9cc70e7c07ae4bb8a52c3f4eb06d66634
vikalp-tomer <vikalptomar15@gmail.com>
2024-04-22 07:36:41 +0530

    add ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,20 @@
+const Message = () => {
+  return (
+    <div className={`chat chat-end`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" src={""} />
+        </div>
+      </div>
+      <div
+        className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        {formattedTime}
+      </div>
+    </div>
+  );
+};
+export default Message;
